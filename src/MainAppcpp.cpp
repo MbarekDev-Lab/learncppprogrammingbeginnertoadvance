667627cb68dbcede37f707af90b70900d6a4902c
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-04-17 07:38:12 +0200

    Convert to lowercase by adding 32

--- src/MainAppcpp.cpp
+++ src/MainAppcpp.cpp
@@ -19,13 +19,12 @@ int main() {
 
 		string str = "WeLcOmE7";
 
-		for (int i = 0; str[i] != '\0';i++) {
-
-			if (str[i] >= 65 && str[i]<= 90) {
-				str[i] = str[i] + 32;
+		for (int i = 0; str[i] != '\0'; i++) {
+			if (str[i] >= 97; && str[i] <= 122) {  // Check if character is uppercase A-Z
+				str[i] -= 32;           // Convert to lowercase by adding 32
 			}
 		}
-		cout << "str : " << str << endl;
+		cout << "str : " << str << endl
 
 
 
